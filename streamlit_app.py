cd40210c35464b2353c40e9152152bafea0f7f78
PatrickMari <70908504+PatrickMari@users.noreply.github.com>
2023-09-26 10:02:20 +0800

    Update streamlit_app.py
    Added text menu bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Eggs and Bacon')
+streamlit.text('Eggs and Hotdog')
+streamlit.text('Beef Tapa')
