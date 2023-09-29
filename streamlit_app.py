cad4d43ad467adfd688d8901cd8e84eee1889720
PatrickMari <70908504+PatrickMari@users.noreply.github.com>
2023-09-29 13:35:08 +0800

    Update streamlit_app.py
    lesson 12 if else function

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,16 +49,22 @@ streamlit.stop()
 
 #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list") 
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-#allo to add
+#######################################################
+#allow users to add
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('The user entered ', add_my_fruit)
-
-#this will not work correctly but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
